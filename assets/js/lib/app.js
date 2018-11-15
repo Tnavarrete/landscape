d916be5190e9090de6243011a770b0a2cf3f04a6
kibernum <tamaranavac@gmail.com>
2018-11-15 19:34:24 -0300

    first commit

---
+++ assets/js/lib/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+//Aqui va a ejecutarse el código una vez que este cargado
+
+$('.card').click(function(){
+//Aqui agreagmos la función que quita o agraga la clase ´card--open'
+ $(this).toggleClass('card--open');
+});
+
+
+//like en el corazon
+
+$('.card__like').click(function(event){
+	event.preventDefault();
+	event.stopPropagation();
+
+ $(this).toggleClass('card__like--red');
+});
+
+$('.card__follow-btn').click(function(event){
+	event.preventDefault();
+	event.stopPropagation();
+
+ $(this).toggleClass('card__follow-btn--following');
+
+
+ if($(this).text() ===  'Seguir'){
+ 	$(this).text('Siguiendo');
+ }else{
+ 	$(this).text('Seguir');
+ }
+});
+
+$('.create__select').change(function(){
+
+	var image = $(this).val();
+	$('.create_img').attr('src',image);
+	//console.log('change' , $(event.currentTarget).val());
+    //console.log('change' , $(this).val());
+});
+
+
+
+});
